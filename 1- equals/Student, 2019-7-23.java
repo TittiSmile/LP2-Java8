80e7ae4b577a5d5983ec81d26fffb58b9d0efc54
TittiSmile <ghittisp@gmail.com>
2022-03-03 21:42:59 +0100

    aggiunta cartella equals

---
+++ 1- equals/Student, 2019-7-23.java
@@ -0,0 +1,85 @@
+package com.company;
+
+public class Student {
+    private String nome;
+    private long matricola;
+    Student(String nome, long matricola){
+        this.nome=nome;
+        this.matricola=matricola;
+    }
+
+    //b)
+   /* @Override
+    public boolean equals(Object obj){
+        if(obj==null)
+            return false;
+        if(this.getClass()!=obj.getClass())
+            return false;
+        Student s = (Student) obj;
+        if(checkDispari(matricola)==checkDispari(s.matricola))
+            return true;
+        if(checkPari(matricola)==checkPari(s.matricola))
+            return true;
+        return false;
+    }*/
+
+   //d)
+   @Override
+   public boolean equals(Object obj){
+       if(obj==null)
+           return false;
+       if(this.getClass()!=obj.getClass())
+           return false;
+       Student s = (Student) obj;
+       return this.matricola==s.matricola &&( ((this.nome==null) && (s.nome==null)) || ( (this.nome)==s.nome  )  );
+   }
+
+    boolean checkPari(long matricola){
+        if(matricola%2==0)
+            return true;
+        return false;
+    }
+
+    boolean checkDispari(long matricola){
+        if(matricola%2!=0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return this.nome + " " + this.matricola;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    // write your code here
+        Student s = new Student("pippo", 1000);
+        Student s2 = new Student("pippo", 1020);
+        Student s3 = new Student("pippo", 1003);
+        Student s4 = new Student("pippo", 1001);
+        //System.out.println(s4.equals(s3));
+
+        Student s5 = new Student(null, 1000);
+        Student s6 = new Student(null, 1000);
+        Student s7 = new Student("pippo", 1001);
+        Student s8 = new Student("pippo", 1001);
+        System.out.println(s8.equals(s7));
+
+
+    }
+}
