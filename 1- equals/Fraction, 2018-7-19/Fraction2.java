94ca704ad936cebacae973af42fc34e274d86115
TittiSmile <46401996+TittiSmile@users.noreply.github.com>
2022-03-08 19:38:11 +0100

    Update Fraction2.java

--- 1- equals/Fraction, 2018-7-19/Fraction2.java
+++ 1- equals/Fraction, 2018-7-19/Fraction2.java
@@ -10,15 +10,12 @@ class Fraction2 {
         this.denominatore=denom;
     }
 
-    int MasComDiv(){    //ma se questa funzione la chiamo dalla sottoclasse, this.numeratore è della sottoclasse?
-                        //Sì!! questo perchè hai fatto la super che ti va ad associare il num e denum del costruttore della sottoclasse
-                        //come this di numeratore e denominatore. chiaramente, nella superclasse sarà this della classe fraction
+    int MasComDiv(){    
         int MCD = BigInteger.valueOf(this.numeratore).gcd(BigInteger.valueOf(this.denominatore)).intValue();
         return MCD;
     }
 
-    static int MasComDiv2(int num, int denom ){     //altra funzione perchè così non si hanno problemi col this e chi cazzo è cosa. maledetto java
-                                                    //statico perchè così è la prima cosa che viene buttata in memoria, senza non avrei potuto chiamare il super di sotto
+    static int MasComDiv2(int num, int denom ){     
         int MCD = BigInteger.valueOf(num).gcd(BigInteger.valueOf(denom)).intValue();
         return MCD;
     }
@@ -44,31 +41,28 @@ class Fraction2 {
         return newDenom;
     }
 
-    public boolean equals(Object obj){      //se gli passo un fraction funziona
+    public boolean equals(Object obj){     
         if(obj==null){
             return false;
         }
 
         Fraction2 f = (Fraction2)obj;
         System.out.println(this.scomponiNum() + " " + f.numeratore + " "+ f.denominatore + " "+ this.scomponiDenom());
-        //if(this.numeratore==f.numeratore && this.denominatore == f.denominatore){
         if(this.scomponiNum() == f.numeratore && this.scomponiDenom() == f.denominatore){
             return true;
         }
-        //System.out.println(f.numeratore + " " + this.numeratore + " "+ f.denominatore + " "+ this.denominatore);
-
         return false;
     }
 
 
     @Override
-    public String toString() {      //RICORDA che viene ereditato dalla sottoclasse!!!
+    public String toString() {     
         return numeratore + "/" + denominatore;
     }
 
 
     void print(){
-        System.out.println(this.numeratore + "  "+ this.denominatore); //qui num e denom sono 12 e 30. perchè in equals della sottoclasse sono 2 e 5?
+        System.out.println(this.numeratore + "  "+ this.denominatore); 
     }
 }
 
@@ -79,10 +73,7 @@ class ReducedFraction2 extends Fraction2{
     public int numeratore;
     public int denominatore;
     ReducedFraction2(int num, int denom){
-        super(num/MasComDiv2(num,denom),denom/MasComDiv2(num,denom));   //e che è? semplicemente,visto che l'aggiornamento del numeratore e
-                                                                                    // denominatore si fa sotto e non si vanno ad aggiornare i valori per il
-                                                                                   // metodo times, buttiamo le cose aggiornate direttamente nella chiamata
-                                                                                   //a super così siamo tutti più felici
+        super(num/MasComDiv2(num,denom),denom/MasComDiv2(num,denom));  
 
         //riduco la frazione
         numeratore = num/MasComDiv2(num, denom);
@@ -98,7 +89,7 @@ class ReducedFraction2 extends Fraction2{
         if(super.numeratore == this.numeratore && super.denominatore == this.denominatore){
             return true;
         }
-        System.out.println(super.numeratore + "  "+ this.numeratore + " "+ super.denominatore+ "  "+ this.denominatore); //come mai il super di denominatore è 5? non dvorebbe essere 30?
+        System.out.println(super.numeratore + "  "+ this.numeratore + " "+ super.denominatore+ "  "+ this.denominatore); 
         return false;
     }
 
