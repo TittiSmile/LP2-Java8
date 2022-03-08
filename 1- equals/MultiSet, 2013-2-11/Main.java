b4c76ee3f624e855137f4ec849a001cdfb0a4844
TittiSmile <46401996+TittiSmile@users.noreply.github.com>
2022-03-08 19:40:00 +0100

    Update Main.java

--- 1- equals/MultiSet, 2013-2-11/Main.java
+++ 1- equals/MultiSet, 2013-2-11/Main.java
@@ -7,17 +7,12 @@ public class Main {
         MultiSet<Integer> s1 = new MultiSet<Integer>();
         MultiSet<Integer> s2 = new MultiSet<Integer>();
         s1.add(5); s1.add(7); s1.add(5);
-        //s2.add(5); s2.add(5); s2.add(7);
         s2.add(5); s2.add(5); s2.add(7);
 
         for (Integer n: s1)
             System.out.println(n);
         System.out.println(s1.equals(s2));
 
-
-        /* System.out.println(s1);
-        Boolean b = s1.contElement(s2);
-        System.out.println(b);*/
     }
 
 }
